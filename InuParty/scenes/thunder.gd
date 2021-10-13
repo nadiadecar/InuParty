9ffaab004c04f18786f2f4b9e42550fa25d57751
VanessaGaete <vanessa.gaete@ug.uchile.cl>
2021-10-13 17:28:34 -0300

    Cambia tamaño de imagen y de colision cuando el objeto se utiliza

--- InuParty/scenes/thunder.gd
+++ InuParty/scenes/thunder.gd
@@ -3,11 +3,8 @@ extends KinematicBody2D
 var lineal_vel = Vector2.ZERO
 var SPEED = 300
 var ACCELERATION = 5
-var GRAVITY = 400
+var GRAVITY = 10
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 onready var playback = $AnimationTree.get("parameters/playback")
 
 func _ready() -> void:
@@ -16,7 +13,16 @@ func _ready() -> void:
 
 
 func _physics_process(delta) -> void:
+	
+	lineal_vel = move_and_slide(lineal_vel, Vector2.UP)
+	lineal_vel.y += GRAVITY * delta
+	var on_floor = is_on_floor()
+	
+	var target_vel = Input.get_action_strength("right_obj") - Input.get_action_strength("left_obj")
+	
+	lineal_vel.x = move_toward(lineal_vel.x, target_vel * SPEED, ACCELERATION)
+	
 	# ANIMATIONS
-	if Input.is_action_just_pressed("enter"):
+	if Input.is_action_just_pressed("fast_fall"):
 		playback.travel("Active")
 
