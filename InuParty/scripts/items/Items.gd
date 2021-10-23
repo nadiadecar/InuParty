057ec5136023f9c16ed04e39d2aa0d894d78594a
VanessaGaete <vanessa.gaete@ug.uchile.cl>
2021-10-23 19:19:02 -0300

    Hace que los items desaparezcan luego de terminar ocn su animación

--- InuParty/scripts/items/Items.gd
+++ InuParty/scripts/items/Items.gd
@@ -9,23 +9,31 @@ var itemList = [Thunder, Bomb, Water, Bone, Thor]
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var actualItem
+onready var timer = $Timer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	itemList.shuffle()
 	print(itemList)
-	var actualItem= itemList[0].instance()
+	actualItem= itemList[0].instance()
 	self.add_child(actualItem)
 	actualItem.global_position = global_position
 	
+	
 func _physics_process(delta) -> void:
 	if Input.is_action_just_pressed("fast_fall") or Input.is_action_just_pressed("change_obj"):
-		itemList.shuffle()
-		var actualItem= itemList[0].instance()
-		self.add_child(actualItem)
-		actualItem.global_position = global_position
+		timer.set_wait_time( 1.5 )
+		timer.connect("timeout", self, "_on_Timer_timeout")
+		timer.start()
 
+func _on_Timer_timeout():
+	timer.stop()
+	self.remove_child(actualItem)
+	itemList.shuffle()
+	self.remove_child(actualItem)
+	actualItem= itemList[0].instance()
+	self.add_child(actualItem)
+	actualItem.global_position = global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
