400d7468ac33c3e4cb74cf2817373058938555cb
VanessaGaete <vanessa.gaete@ug.uchile.cl>
2021-10-23 19:31:48 -0300

    arregla problema de cambio de objetos

--- InuParty/scripts/items/Items.gd
+++ InuParty/scripts/items/Items.gd
@@ -21,8 +21,13 @@ func _ready():
 	
 	
 func _physics_process(delta) -> void:
-	if Input.is_action_just_pressed("fast_fall") or Input.is_action_just_pressed("change_obj"):
+	if Input.is_action_just_pressed("fast_fall"):
 		timer.set_wait_time( 1.5 )
+		timer.connect("timeout", self, "_on_Timer_timeout")
+		timer.start()
+		
+	if Input.is_action_just_pressed("change_obj"):
+		timer.set_wait_time( 0.05 )
 		timer.connect("timeout", self, "_on_Timer_timeout")
 		timer.start()
 
@@ -30,7 +35,6 @@ func _on_Timer_timeout():
 	timer.stop()
 	self.remove_child(actualItem)
 	itemList.shuffle()
-	self.remove_child(actualItem)
 	actualItem= itemList[0].instance()
 	self.add_child(actualItem)
 	actualItem.global_position = global_position
