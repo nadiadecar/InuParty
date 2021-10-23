4009108c5ec722e20cbd44c2db38854a532063c1
VanessaGaete <vanessa.gaete@ug.uchile.cl>
2021-10-23 18:06:54 -0300

    Cae un item al azar desde arriba

--- InuParty/scripts/items/Items.gd
+++ InuParty/scripts/items/Items.gd
@@ -5,7 +5,7 @@ const Bomb = preload("res://scenes/items/bomb.tscn")
 const Bone = preload("res://scenes/items/Bone.tscn")
 const Thor = preload("res://scenes/items/objeto_thor.tscn")
 
-
+var itemList = [Thunder, Bomb, Water, Bone, Thor]
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -13,15 +13,18 @@ const Thor = preload("res://scenes/items/objeto_thor.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var bombItem= Bomb.instance()
-	self.add_child(bombItem)
-	bombItem.global_position = global_position
+	itemList.shuffle()
+	print(itemList)
+	var actualItem= itemList[0].instance()
+	self.add_child(actualItem)
+	actualItem.global_position = global_position
 	
 func _physics_process(delta) -> void:
-	if Input.is_action_just_pressed("fast_fall"):
-		var thunderItem= Thunder.instance()
-		self.add_child(thunderItem)
-		thunderItem.global_position = global_position
+	if Input.is_action_just_pressed("fast_fall") or Input.is_action_just_pressed("change_obj"):
+		itemList.shuffle()
+		var actualItem= itemList[0].instance()
+		self.add_child(actualItem)
+		actualItem.global_position = global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
