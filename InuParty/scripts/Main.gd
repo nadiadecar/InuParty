192c4be4e20bce8fd0d3e98b44a9023b8c7e6283
VanessaGaete <vanessa.gaete@ug.uchile.cl>
2021-10-23 17:55:38 -0300

    Revert "Inicio de inventario 2.0"
    This reverts commit 91d7a838fd51c29d803c9e2125a0db8ad302a3ad.

--- InuParty/scripts/Main.gd
+++ InuParty/scripts/Main.gd
@@ -1,23 +1,5 @@
-extends TextureRect
+extends Node2D
 
-#const full = Rect2()
-#const empty = Rect2()
-
-onready var holder = $Holder
-
-var item: Node = null setget set_item
-
-func set_item(value):
-	item = value
-	var atlas = texture as AtlasTexture
-	if item: 
-		item.get_parent().remove_child(item)
-		holder.add_child(item)
-	#	atlas.region = full
-	#else:
-	#	atlas.reguin = empty
-
-#func _ready() -> void: 
-#	_update_slot()
-
-#func _update_slot():
+func _ready():
+	get_tree().change_scene("res://scenes/stages/stage1.tscn")
+	
