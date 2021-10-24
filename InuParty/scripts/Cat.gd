1448eb917d1b0646a8e4b6d481611b8ea2da59bf
VanessaGaete <vanessa.gaete@ug.uchile.cl>
2021-10-24 18:08:18 -0300

    Items interactuan con gato y perro

--- InuParty/scripts/Cat.gd
+++ InuParty/scripts/Cat.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var lineal_vel = Vector2.ZERO
-var SPEED = 200
+var SPEED = 180
 var ACCELERATION = 1
 var GRAVITY = 400
 
@@ -12,6 +12,7 @@ var player_distance
 
 onready var playback = $AnimationTree.get("parameters/playback")
 onready var player = get_parent().get_node("Player")
+onready var timer = $WaterTimer
 
 func _ready() -> void:
 	$AnimationTree.active = true
@@ -28,9 +29,25 @@ func _physics_process(delta) -> void:
 	if (player_distance < NEAR_DISTANCE_THRESHOLD):
 		#pass
 		get_tree().change_scene("res://scenes/ui/win_menu.tscn")
+		
+	if $RayCast2D.is_colliding():
+		var colObj = $RayCast2D.get_collider()
+		print(colObj)
+		if colObj.is_in_group("cat_nerfs"):
+			SPEED = 0
+			lineal_vel.x = move_toward(lineal_vel.x, target_vel * SPEED, ACCELERATION)
+			timer.set_wait_time( 5 )
+			timer.connect("timeout", self, "_on_Timer_timeout")
+			timer.start()
 	
 	#ANIMATIONS
 	if abs(lineal_vel.x) <= 0:
 		playback.travel("Idle")
 	if 0 < abs(lineal_vel.x):
 		playback.travel("Walk")
+
+func _on_Timer_timeout():
+	timer.stop()
+	SPEED = 180
+	var target_vel = 1
+	lineal_vel.x = move_toward(lineal_vel.x, target_vel * SPEED, ACCELERATION)
