38b5d83c4d10be6f6c79de0017585ea117ab8411
Sebastián <szapata@dcc.uchile.cl>
2021-12-28 15:27:39 -0300

    correcciones en tutorial y créditos

--- InuParty/scripts/ui/credits.gd
+++ InuParty/scripts/ui/credits.gd
@@ -17,7 +17,7 @@ func _ready():
 	back_button.connect("pressed", self, "on_back_pressed")
 	
 	tween.interpolate_property(creditsLabel, "rect_position",
-		Vector2(0, 750), Vector2(0, 700 - creditsLabel.rect_size.y), 7.5,
+		Vector2(0, 750), Vector2(0, 700 - creditsLabel.rect_size.y), 17.5,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
 
